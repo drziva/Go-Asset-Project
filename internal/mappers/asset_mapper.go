0827c4a5a2dc96a1844d2fec271ebbd81ac4a0b3
Nikola Zivaljevic <nikola.zivaljevic@vega.local>
2026-04-27 13:01:16 +0200

    feat(assets): implement download EP, fix update endpoints

--- internal/mappers/asset_mapper.go
+++ internal/mappers/asset_mapper.go
@@ -1,6 +1,7 @@
 package mappers
 
 import (
+	"fmt"
 	"go-project/internal/dto"
 	"go-project/internal/models"
 )
@@ -8,8 +9,26 @@ import (
 func ToAssetResponse(asset models.Asset) dto.AssetResponse {
 	return dto.AssetResponse{
 		ID:          asset.ID,
-		Name:        asset.Name,
+		FileName:    asset.FileName,
+		FileSize:    formatFileSize(asset.FileSize),
+		MimeType:    asset.MimeType,
 		Description: asset.Description,
 		UserID:      asset.UserID,
 	}
+}
+
+func formatFileSize(size int64) string {
+	const unit = 1024
+
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
+	}
+
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+
+	return fmt.Sprintf("%.1f %cB", float64(size)/float64(div), "KMGTPE"[exp])
 }
